c0485f4ffdf2acb9b51b0446915e8490cbbbc77e
laetitiapafundi <letizia.pafundi@gmail.com>
2024-06-02 21:30:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,19 +6,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const scrollToSection = (index) => {
         isScrolling = true;
         sections[index].scrollIntoView({ behavior: 'smooth', block: 'start' });
-        setTimeout(() => { isScrolling = false; }, 1000); // Delay to prevent excessive scrolling
+        setTimeout(() => { isScrolling = false; }, 1000);
     };
 
     window.addEventListener('wheel', (event) => {
         if (isScrolling) return;
         if (event.deltaY > 0) {
-            // Scrolling down
             if (currentIndex < sections.length - 1) {
                 currentIndex++;
                 scrollToSection(currentIndex);
             }
         } else {
-            // Scrolling up
             if (currentIndex > 0) {
                 currentIndex--;
                 scrollToSection(currentIndex);
@@ -26,18 +24,15 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    // Dissolution de l'opacité du loader et animation des classes tada
     setTimeout(() => {
         gsap.to('.loader', { duration: 1, opacity: 0, onComplete: () => {
-            document.getElementById("loader").style.display = "none"; // hide loader after fading out
+            document.getElementById("loader").style.display = "none";
 
-            // Animer les classes tada immédiatement après la disparition du loader
             gsap.fromTo('.tada-first', { y: 40, opacity: 0 }, { y: 0, opacity: 1, duration: 1, ease: 'power2.out' });
             gsap.fromTo('.tada-second', { y: 40, opacity: 0 }, { y: 0, opacity: 1, duration: 1, ease: 'power2.out', delay: 0.5 });
         }});
     }, 1200);
 
-    // Initialiser l'animation Lottie
     const lottieArrow = document.getElementById('lottie-arrow');
     lottie.loadAnimation({
         container: lottieArrow,
